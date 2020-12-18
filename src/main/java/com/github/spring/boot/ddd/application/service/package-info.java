b652e98c4897f7dc4e71631a266a1fa4de71a829
shaopro <shaopro@qq.com>
2020-12-18 11:21:15 +0800

    Initial commit

---
+++ src/main/java/com/github/spring/boot/ddd/application/service/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ *@author 石少东
+ *@date 2020-12-18 11:21
+ *@since 1.0
+*/
+
+
+package com.github.spring.boot.ddd.application.service;
---
+++ src/test/java/com/github/spring/boot/ddd/SpringBootDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.spring.boot.ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
